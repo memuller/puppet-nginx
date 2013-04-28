dc08a5c4e8f395987ff3b3b46a7c767f5a456e19
Matheus E. Muller <hello@memuller.com>
2013-04-28 19:17:20 -0300

    Updates w/ several nginx templates.

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,30 +37,73 @@ class nginx (
 	file { '/etc/nginx/globals':
 		ensure => directory
 	}
-
+	# PHP upstream.
 	file { '/etc/nginx/globals/php.conf':
 		ensure => present,
 		content => template('nginx/php.conf.erb')
 	}
 
+	# Simple PHP app; redirects all non-found requests to index.php .
 	file { '/etc/nginx/globals/php-simple.conf':
 		ensure => present,
 		content => template('nginx/php-simple.conf.erb')
 	}
 
+	# Wordpress, installed w/ separate directories for wp-content and wordpress.
 	file { '/etc/nginx/globals/wordpress-subdirectory.conf':
 		ensure => present,
 		content => template('nginx/wordpress-subdirectory.conf.erb')
 	}
 
+	# Wordpress MU.
+	file { '/etc/nginx/globals/wordpress-mu.conf':
+		ensure => present,
+		content => template('nginx/wordpress-mu.conf.erb')
+	}
+
+	# GZIP.
+	file { '/etc/nginx/globals/gzip.conf':
+		ensure => present,
+		content => template('nginx/gzip.conf.erb')
+	}
+
+	# No-transform header (disencourages mobile carries from transforming content).
+	file { '/etc/nginx/globals/no-transform.conf':
+		ensure => present,
+		content => template('nginx/no-transform.conf.erb')
+	}
+
+	# File access restrictions (robots, favicon, hidden files, source files)
 	file { '/etc/nginx/globals/restrictions.conf':
 		ensure => present,
 		content => template('nginx/restrictions.conf.erb')
 	}
 
-	file { '/etc/nginx/globals/wordpress-mu.conf':
+	#== Cache handlers.
+	# Media (image/video/document) handler.
+	file { '/etc/nginx/globals/cache-media.conf':
 		ensure => present,
-		content => template('nginx/wordpress-mu.conf.erb')
+		content => template('nginx/cache-media.conf.erb')
+	}
+	# Javascript and CSS handlers.
+	file { '/etc/nginx/globals/cache-app.conf':
+		ensure => present,
+		content => template('nginx/cache-app.conf.erb')
+	}
+	# Web documents (HTML/Manifest) handlers.
+	file { '/etc/nginx/globals/cache-html.conf':
+		ensure => present,
+		content => template('nginx/cache-html.conf.erb')
+	}
+	# Cache-busting using filenames (file.20120212.css => file.css?20120212)
+	file { '/etc/nginx/globals/cache-buster.conf':
+		ensure => present,
+		content => template('nginx/cache-buster.conf.erb')
+	}
+	# Caches file descriptors.
+	file { '/etc/nginx/globals/cache-file-descriptors.conf':
+		ensure => present,
+		content => template('nginx/cache-file-descriptors.conf.erb')
 	}
 
 }
